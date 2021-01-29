96ffbb5080a4286680390f5ec279b282bb0ca5bf
Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>
2021-01-29 16:08:02 +0100

    initial commit
    Signed-off-by: Wenzel Reichmuth <wenzel.reichmuth@vraktion.com>

---
+++ BLEServiceChain.cpp
@@ -0,0 +1,80 @@
+#include "BLEServiceChain.h"
+
+BLEServiceChain::BLEServiceChain(): service(NULL), next(NULL){}
+
+BLEServiceChain::BLEServiceChain(BLEService *service):service(service), next(NULL){}
+
+void BLEServiceChain::add(BLEService *service)
+{
+    if (this->service == NULL)
+    {
+        this->service = service;
+    }
+    else if (this->next == NULL)
+    {
+        this->next = new BLEServiceChain(service);
+    }
+    else
+    {
+        this->next->add(service);
+    }
+}
+
+void BLEServiceChain::checkWriteAccess(const GattWriteCallbackParams *params){
+    if(this->service == NULL) return;
+    //Todo check for writable chars first
+    bool res = this->service->checkWriteAccess(params);
+    if (!res && this->next != NULL)
+    {
+        this->next->checkWriteAccess(params);
+    }
+}
+
+void BLEServiceChain::checkReadAccess(const GattReadCallbackParams *params){
+    if(this->service == NULL) return;
+    //Todo check for readable chars first
+    bool res = this->service->checkReadAccess(params);
+    if (!res && this->next != NULL)
+    {
+        this->next->checkReadAccess(params);
+    }
+}
+
+void BLEServiceChain::checkNotifyRegistrations(GattAttribute::Handle_t handle, bool enable){
+    if(this->service == NULL) return;
+    //Todo check for notifiable chars first
+    bool res = this->service->checkNotifyRegistrations(handle, enable);
+    if (!res && this->next != NULL)
+    {
+        this->next->checkNotifyRegistrations(handle, enable);
+    } 
+}
+
+void BLEServiceChain::init(){
+    if(this->service == NULL) return;
+    this->service->init();
+    if (this->next != NULL)
+    {
+        this->next->init();
+    } 
+}
+
+void BLEServiceChain::pastBleInit(){
+    if(this->service == NULL) return;
+    this->service->pastBleInit();
+    if (this->next != NULL)
+    {
+        this->next->pastBleInit();
+    } 
+}
+
+void BLEServiceChain::initCustomGattServices(){
+    if(this->service == NULL) return;
+    BLE &ble = BLE::Instance();
+    ble.gattServer().addService(*this->service->getGattService());
+
+    if (this->next != NULL)
+    {
+        this->next->initCustomGattServices();
+    }
+}
---
+++ BLEServiceChain.h
@@ -0,0 +1,31 @@
+#ifndef BLE_SERVICE_CHAIN_H
+#define BLE_SERVICE_CHAIN_H
+
+#include "mbed.h"
+#include "ble/BLE.h"
+#include "ble/Gap.h"
+#include "BLEService.h"
+
+class BLEServiceChain
+{
+public:
+
+    BLEServiceChain();
+    BLEServiceChain(BLEService *service);
+
+    void add(BLEService *service);
+    void checkWriteAccess(const GattWriteCallbackParams *params);
+    void checkReadAccess(const GattReadCallbackParams *params);
+    void checkNotifyRegistrations(GattAttribute::Handle_t handle, bool enable);
+    void init();
+    void pastBleInit();
+    void initCustomGattServices();
+
+private:
+
+    BLEService *service;
+    BLEServiceChain *next;
+
+};
+
+#endif //BLE_SERVICE_CHAIN_H
